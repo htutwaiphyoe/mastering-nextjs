4604f1a71135a0d4078ff7e0859d2d9a20c13305
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-06 20:16:52 +0630

    #add: navigating programmatically

--- file-based-routing/pages/clients/[name]/index.js
+++ file-based-routing/pages/clients/[name]/index.js
@@ -3,9 +3,16 @@ import { useRouter } from "next/router";
 const Client = (props) => {
     const router = useRouter();
     console.log(router);
+    const onClickHandler = () => {
+        router.push({
+            pathname: "/clients/[name]/projects",
+            query: { name: "mtu" },
+        });
+    };
     return (
         <main>
             <h1>Client</h1>
+            <button onClick={onClickHandler}>Go to projects</button>
         </main>
     );
 };
