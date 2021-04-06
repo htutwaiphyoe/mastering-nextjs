59d8d8b21ef3675778d689307c1f15497018ce2b
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-06 13:26:42 +0630

    #add: creating nested paths & routes

---
+++ file-based-routing/pages/blogs/index.js
@@ -0,0 +1,9 @@
+const Blogs = (props) => {
+    return (
+        <main>
+            <h1>Blogs</h1>
+        </main>
+    );
+};
+
+export default Blogs;
---
+++ file-based-routing/pages/blogs/new.js
@@ -0,0 +1,9 @@
+const New = (props) => {
+    return (
+        <main>
+            <h1>New</h1>
+        </main>
+    );
+};
+
+export default New;
