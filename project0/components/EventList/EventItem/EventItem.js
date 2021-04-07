20302a770a46370846b2be2e80202951818864e0
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-07 20:38:11 +0630

    #add: Button & Icons Components

--- project0/components/EventList/EventItem/EventItem.js
+++ project0/components/EventList/EventItem/EventItem.js
@@ -1,4 +1,7 @@
-import Link from "next/link";
+import AddressIcon from "../../shared/icons/AddressIcon";
+import ArrowRightIcon from "../../shared/icons/ArrowRightIcon";
+import DateIcon from "../../shared/icons/DateIcon";
+import Button from "../../shared/Button/Button";
 import classes from "./EventItem.module.css";
 
 const EventItem = ({ title, id, image, location, date }) => {
@@ -17,14 +20,21 @@ const EventItem = ({ title, id, image, location, date }) => {
                 <div className={classes.summary}>
                     <h2>{title}</h2>
                     <div className={classes.date}>
+                        <DateIcon />
                         <time>{formatedDate}</time>
                     </div>
                     <div className={classes.address}>
+                        <AddressIcon />
                         <address>{formatedLocation}</address>
                     </div>
                 </div>
                 <div className={classes.actions}>
-                    <Link href={link}>Explore event</Link>
+                    <Button link={link}>
+                        <span>Explore event</span>
+                        <span className={classes.icon}>
+                            <ArrowRightIcon />
+                        </span>
+                    </Button>
                 </div>
             </div>
         </li>
