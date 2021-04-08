3a74a020d8dd74a8d7fdbb94fd02e2b00e03ce53
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-08 16:08:52 +0630

    #add: configuring options of getStaticProps

--- data-fetching/pages/index.js
+++ data-fetching/pages/index.js
@@ -14,10 +14,21 @@ const Home = (props) => {
 };
 
 export async function getStaticProps(context) {
-    console.log("generated");
     const filePath = path.join(process.cwd(), "dev_data", "data.json");
     const jsonData = await fs.readFile(filePath, "utf-8");
     const data = JSON.parse(jsonData);
+    if (data.length === 0) {
+        return {
+            notFound: true,
+        };
+    }
+    if (!data) {
+        return {
+            redirect: {
+                destination: "/home",
+            },
+        };
+    }
     return {
         props: {
             products: data.products,
