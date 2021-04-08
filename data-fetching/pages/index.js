9d5573abcfd6aed6efb72538c6d17cbafbf00e2d
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-08 14:49:59 +0630

    #add: utilizing Incremental Static Generation (ISG)

--- data-fetching/pages/index.js
+++ data-fetching/pages/index.js
@@ -14,6 +14,7 @@ const Home = (props) => {
 };
 
 export async function getStaticProps(context) {
+    console.log("generated");
     const filePath = path.join(process.cwd(), "dev_data", "data.json");
     const jsonData = await fs.readFile(filePath, "utf-8");
     const data = JSON.parse(jsonData);
@@ -21,6 +22,7 @@ export async function getStaticProps(context) {
         props: {
             products: data.products,
         },
+        revalidate: 10,
     };
 }
 export default Home;
