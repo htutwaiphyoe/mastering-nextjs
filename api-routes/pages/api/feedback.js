89d91dd0cf14d8b4addac9f93a1466d8cbd39c1d
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-14 23:30:41 +0630

    #add: post api

--- api-routes/pages/api/feedback.js
+++ api-routes/pages/api/feedback.js
@@ -1,5 +1,27 @@
+import fs from "fs/promises";
+import path from "path";
+
 const handler = (req, res) => {
-    res.status(200).json({
+    if (req.method === "POST") {
+        const { email, feedback } = req.body;
+        const data = {
+            id: new Date().toISOString() * Math.random().toString(36),
+            email,
+            feedback,
+        };
+        const filePath = path.join(process.cwd(), "data.json", "data");
+        const fileData = JSON.parse(await fs.readFile(filePath));
+        fileData.push(data);
+        await fs.writeFile(filePath, fileData);
+        res.status(201).json({
+            status: "success",
+            data: {
+                feedback: data,
+            }
+        })
+    }
+
+    return res.status(200).json({
         status: "ok",
         message: "success",
     });
--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,3 +1,47 @@
-const Home = () => <div>Hello, Nextjs API</div>;
+import { useRef } from "react";
+
+const Home = () => {
+    const emailRef = useRef("");
+    const feedbackRef = useRef("");
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        const email = emailRef.current.value;
+        const feedback = feedbackRef.current.value;
+        fetch({
+            url: "http://localhost:3000/api/feedback",
+            method: "POST",
+            body: JSON.stringify({ email, feedback }),
+        });
+    };
+    return (
+        <main>
+            <form onSubmit={onSubmitHandler}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Email"
+                        ref={emailRef}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="feedback">Feedback</label>
+                    <textarea
+                        name="feedback"
+                        id="feedback"
+                        rows="10"
+                        cols="30"
+                        placeholder="Feedback"
+                        ref={feedbackRef}
+                    />
+                </div>
+                <button type="submit">Send Feedback</button>
+            </form>
+        </main>
+    );
+};
 
 export default Home;
