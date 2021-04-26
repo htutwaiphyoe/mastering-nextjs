da16a28589386cdf907e21ad423297e49c89372d
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-26 13:55:00 +0630

    #add: notification component

--- project0/pages/_app.js
+++ project0/pages/_app.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 
 import Layout from "../components/Layout/Layout";
+import Notification from "../components/shared/Notification/Notification";
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
@@ -10,6 +11,7 @@ function App({ Component, pageProps }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
             <Component {...pageProps} />
+            <Notification title="test" status="error" message="test" />
         </Layout>
     );
 }
