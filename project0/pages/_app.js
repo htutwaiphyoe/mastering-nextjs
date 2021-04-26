eca5344698f8fb132f1837278279e1d806a34610
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-26 14:10:31 +0630

    #add: creating notification context and provider

--- project0/pages/_app.js
+++ project0/pages/_app.js
@@ -2,17 +2,20 @@ import Head from "next/head";
 
 import Layout from "../components/Layout/Layout";
 import Notification from "../components/shared/Notification/Notification";
+import { NotificationContextProvider } from "../context/notificationContext";
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
     return (
-        <Layout>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            </Head>
-            <Component {...pageProps} />
-            <Notification title="test" status="error" message="test" />
-        </Layout>
+        <NotificationContextProvider>
+            <Layout>
+                <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                </Head>
+                <Component {...pageProps} />
+                <Notification title="test" status="error" message="test" />
+            </Layout>
+        </NotificationContextProvider>
     );
 }
 
