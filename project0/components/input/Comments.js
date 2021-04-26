a3976d8ad3c9f7af28d53b2d79dc000965dcf10c
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-26 15:50:30 +0630

    #add: showing notification for comments

--- project0/components/input/Comments.js
+++ project0/components/input/Comments.js
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
+import NotificationContext from "../../context/notificationContext";
 import CommentList from "./CommentList";
 import NewComment from "./NewComment";
 import classes from "./Comments.module.css";
 
 function Comments(props) {
     const { eventId } = props;
+    const notificationContext = useContext(NotificationContext);
 
     const [showComments, setShowComments] = useState(false);
     const [comments, setComments] = useState([]);
@@ -13,20 +15,57 @@ function Comments(props) {
     async function toggleCommentsHandler() {
         setShowComments((prevStatus) => !prevStatus);
         if (!showComments) {
+            notificationContext.showNotification({
+                title: "Loading",
+                status: "pending",
+                message: "Loading comments...",
+            });
             const response = await (await fetch("/api/comments/" + eventId)).json();
             setComments(response.comments ? response.comments : []);
+
+            if (response.status === "success") {
+                notificationContext.showNotification({
+                    title: "Success",
+                    status: "success",
+                    message: "Loaded comments successfully",
+                });
+            } else {
+                notificationContext.showNotification({
+                    title: "Error",
+                    status: "error",
+                    message: response.message || "Something went wrong!",
+                });
+            }
         }
     }
 
     async function addCommentHandler(commentData) {
-        const respone = await (
+        notificationContext.showNotification({
+            title: "Loading",
+            status: "pending",
+            message: "Submitting comment...",
+        });
+        const response = await (
             await fetch("/api/comments/" + eventId, {
                 method: "POST",
                 body: JSON.stringify(commentData),
                 headers: { "Content-Type": "application/json" },
             })
         ).json();
-        console.log(respone);
+
+        if (response.status === "success") {
+            notificationContext.showNotification({
+                title: "Success",
+                status: "success",
+                message: "Submitted comment successfully",
+            });
+        } else {
+            notificationContext.showNotification({
+                title: "Error",
+                status: "error",
+                message: response.message || "Something went wrong!",
+            });
+        }
     }
 
     return (
