3af3691d0e1235a61cc62d34b9cacc13eced6264
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-29 17:56:24 +0630

    #fix: reducing bundle size with light build of react-syntax-highlighter

--- nextblog/components/blogs/blogDetail/BlogContent.js
+++ nextblog/components/blogs/blogDetail/BlogContent.js
@@ -1,10 +1,13 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
 
 import BlogHeader from "./BlogHeader";
 import classes from "./BlogContent.module.css";
+
+SyntaxHighlighter.registerLanguage("js", js);
 
 function BlogContent(props) {
     const { blog } = props;
