87165b25587ceab94e17336c63d525247676bc3a
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-29 16:16:15 +0630

    #add: _document

--- nextblog/pages/_app.js
+++ nextblog/pages/_app.js
@@ -8,7 +8,7 @@ function App({ Component, pageProps }) {
         <Layout>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+                <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
             </Head>
             <Component {...pageProps} />
         </Layout>
