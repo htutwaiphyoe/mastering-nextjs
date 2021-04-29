e3827f487d86cf42e886ebfc833aede2150c3019
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-29 13:37:17 +0630

    #add: storing in mongodb server

--- nextblog/pages/api/contact.js
+++ nextblog/pages/api/contact.js
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 async function handler(req, res) {
     if (req.method === "POST") {
         const { name, email, message } = req.body;
@@ -7,10 +9,33 @@ async function handler(req, res) {
                 message: "Invalid inputs",
             });
         }
-        res.status(201).json({
-            status: "success",
-            message: "ok",
-        });
+        let client;
+        try {
+            client = await MongoClient.connect(
+                "mongodb+srv://nextblogs:nextblogs@cluster0.1l9ao.mongodb.net/blogs?retryWrites=true&w=majority"
+            );
+        } catch (e) {
+            return res.status(500).json({
+                status: "error",
+                message: "Cannot connect to database!",
+            });
+        }
+
+        const db = client.db();
+        try {
+            const result = await db.collection("messages").insertOne({ name, email, message });
+            client.close();
+            res.status(201).json({
+                status: "success",
+                message: result.ops[0],
+            });
+        } catch (e) {
+            client.close();
+            return res.status(500).json({
+                status: "error",
+                message: "Cannot store to database!",
+            });
+        }
     }
 }
 
