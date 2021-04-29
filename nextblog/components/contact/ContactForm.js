ccd526d33ef35b58ecf12818e8799cb719c29a51
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-29 15:53:01 +0630

    #add: notification

--- nextblog/components/contact/ContactForm.js
+++ nextblog/components/contact/ContactForm.js
@@ -1,27 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+import Notification from "../ui/Notification";
 import classes from "./ContactForm.module.css";
 
 function ContactForm(props) {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
+    const [notificationStatus, setNotificationStatus] = useState(null);
+
+    useEffect(() => {
+        if (notificationStatus && notificationStatus.status !== "pending") {
+            const timer = setTimeout(() => {
+                setNotificationStatus(null);
+            }, 1000);
+
+            return () => {
+                clearTimeout(timer);
+            };
+        }
+    }, [notificationStatus]);
+
     async function onSubmitHandler(e) {
         e.preventDefault();
-        const respone = await (
-            await fetch("/api/contact", {
-                method: "POST",
-                body: JSON.stringify({
-                    name: name.trim(),
-                    email: email.trim(),
-                    message: message.trim(),
-                }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            })
-        ).json();
-        console.log(respone);
+
+        setNotificationStatus({
+            status: "pending",
+            title: "Sending message...",
+            message: "Your message is on its way.",
+        });
+        try {
+            const response = await (
+                await fetch("/api/contact", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        name: name.trim(),
+                        email: email.trim(),
+                        message: message.trim(),
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+            ).json();
+            setName("");
+            setEmail("");
+            setMessage("");
+            if (response.status === "fail" || response.status === "error") {
+                throw new Error(response.message || "Something went wrong!");
+            }
+            setNotificationStatus({
+                status: "success",
+                title: "Success!",
+                message: "Your message has been sent!.",
+            });
+        } catch (err) {
+            console.log(err);
+            setNotificationStatus({
+                status: "error",
+                title: "Error",
+                message: err.message || "Ah, something went wrong!",
+            });
+        }
     }
     return (
         <section className={classes.contact}>
@@ -63,9 +103,18 @@ function ContactForm(props) {
                     />
                 </div>
                 <div className={classes.actions}>
-                    <button type="submit">Send message</button>
+                    <button type="submit" disabled={notificationStatus ? true : false}>
+                        Send message
+                    </button>
                 </div>
             </form>
+            {notificationStatus && (
+                <Notification
+                    status={notificationStatus.status}
+                    title={notificationStatus.title}
+                    message={notificationStatus.message}
+                />
+            )}
         </section>
     );
 }
